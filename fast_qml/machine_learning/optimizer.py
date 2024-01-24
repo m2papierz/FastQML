9dacfe0f2f0e8394a1a707f908cf2c6f535adf8b
m2papierz <m2papierz@gmail.com>
2024-01-24 12:39:15 +0100

    Add warranty disclaimer in scripts

--- fast_qml/machine_learning/optimizer.py
+++ fast_qml/machine_learning/optimizer.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2024 Mateusz Papierz
+#
+# This file is part of the FastQML library
+#
+# The FastQML library is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the FastQML library, as per Section 15 of the GPL v3.
+
+
 import jax
 import jaxopt
 import pennylane as qml
