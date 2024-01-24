9ef44ded7403703475916e823f96fb3a87503dcc
m2papierz <m2papierz@gmail.com>
2024-01-24 12:56:25 +0100

    Adjust numpy and global params to cpu / jax cpu interface

--- fast_qml/__init__.py
+++ fast_qml/__init__.py
@@ -1,3 +1,27 @@
+import importlib
 from jax import config as j_cfg
 
-j_cfg.update("jax_platform_name", "cpu")
+# Default configuration set to cpu without jax boost
+_device = 'cpu'
+numpy = importlib.import_module("jax.numpy")
+QUBIT_DEV = "default.qubit"
+
+
+def update(device='CPU'):
+    if device == 'cpu':
+        new_np = importlib.import_module("pennylane.numpy")
+        new_qubit_dev = "default.qubit"
+    elif device == 'cpu.jax':
+        j_cfg.update("jax_platform_name", "cpu")
+        new_np = importlib.import_module("jax.numpy")
+        new_qubit_dev = "default.qubit.jax"
+    elif device == 'qpu':
+        raise NotImplementedError
+    else:
+        raise ValueError()
+
+    globals()['numpy'] = new_np
+    globals()['QUBIT_DEV'] = new_qubit_dev
+
+
+__all__ = ['numpy', 'update', 'QUBIT_DEV']
