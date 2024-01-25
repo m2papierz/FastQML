f2fb167db60a32b636764e2a51fc52d629bea5fd
m2papierz <m2papierz@gmail.com>
2024-01-25 22:50:11 +0100

    Refactor VariationalForm to enable custom variational functions handling

--- fast_qml/quantum_circuits/variational_forms.py
+++ fast_qml/quantum_circuits/variational_forms.py
@@ -8,27 +8,20 @@
 #
 # THERE IS NO WARRANTY for the FastQML library, as per Section 15 of the GPL v3.
 
+import warnings
+from abc import abstractmethod
+from typing import Any, Union, Callable
+
 import numpy as np
 import pennylane as qml
 
-from typing import Any
-from abc import abstractmethod
-
+from fast_qml.utils import validate_function_args
 from fast_qml.quantum_circuits.entanglement import EntanglementGenerator
 
 
 class VariationalForm:
-    """
-    Base class for quantum variational forms. Defines a template for creating quantum
-    circuits with variational parameters.
 
-    Parameters:
-    - n_qubits (int): Number of qubits in the quantum circuit.
-    - rotation_blocks (list[str]): List of rotation gate names. Supported values are 'RX', 'RY', and 'RZ'.
-    - controlled_gate (str): Name of the controlled gate. Default is 'CX' (controlled-X gate).
-    - entanglement (str): Type of entanglement to be applied. Default is 'linear'.
-    - reps (int): Number of repetitions in the variational circuit. Default is 1.
-    """
+    _expected_args = ['params']
 
     GATE_MAP = {
         'RX': qml.RX, 'RY': qml.RY, 'RZ': qml.RZ
@@ -37,80 +30,101 @@ class VariationalForm:
     def __init__(
             self,
             n_qubits: int,
+            variational_func: Callable = None,
+            entanglement: Union[str, list[int]] = None,
             rotation_blocks: list[str] = None,
-            controlled_gate: str = 'CX',
-            entanglement: str = 'linear',
-            skip_last_rotation: bool = False,
+            controlled_gate: str = None,
+            uniform_structure: bool = True,
             reps: int = 1
     ):
         self._n_qubits = n_qubits
-        self._skip_last_rotation = skip_last_rotation
         self._reps = reps
+
+        if variational_func is not None:
+            if entanglement is not None or controlled_gate is not None:
+                warnings.warn(
+                    message="If variational_func is given, entanglement "
+                            "and controlled_gate will be ignored.",
+                    category=UserWarning
+                )
+
+            if not validate_function_args(variational_func, self._expected_args):
+                raise ValueError(
+                    f"The variational_form function must "
+                    f"have the arguments: {self._expected_args}"
+                )
+            self._variational_func = variational_func
+        else:
+            self._entanglement = EntanglementGenerator(
+                n_qubits=n_qubits,
+                c_gate=controlled_gate,
+                entanglement=entanglement
+            )
+
+            self._variational_func = self._set_variational_func
 
         self._rotation_blocks = [
             self._validate_gate(block_gate)
             for block_gate in rotation_blocks or ['RY']
         ]
 
-        self._entanglement = EntanglementGenerator(
-            n_qubits=n_qubits,
-            c_gate=controlled_gate,
-            entanglement=entanglement
-        )
+        if uniform_structure:
+            self._func_params_n = self._get_func_params_num()
+
+    @property
+    def params_num(self) -> int:
+        return self._reps * self._func_params_n
+
+    def _get_func_params_num(self) -> int:
+        rot_block_n = len(self._rotation_blocks)
+        aux_params_num = self._n_qubits * rot_block_n
+
+        with qml.tape.QuantumTape() as tape:
+            self._variational_func(np.zeros(shape=aux_params_num))
+
+        return tape.num_params
 
     def _validate_gate(
             self,
             c_gate: str
     ) -> Any:
-        if c_gate in self.GATE_MAP:
-            return self.GATE_MAP[c_gate]
-        else:
+        if c_gate not in self.GATE_MAP:
             raise ValueError(
-                "Invalid controlled gate type. "
+                "Invalid rotation gate type. "
                 "Supported types are 'RX', 'RY', and 'RZ'."
             )
+        return self.GATE_MAP[c_gate]
 
     @abstractmethod
-    def get_params_num(self):
-        pass
-
-    def _validate_params_dims(
+    def _set_variational_func(
             self,
             params: np.ndarray
-    ) -> None:
-        if len(params) != self.get_params_num():
-            raise ValueError(
-                f"Invalid parameters shape. "
-                f"Expected {self.get_params_num()}, got {len(params)}."
-            )
+    ) -> Callable:
+        pass
 
-    @abstractmethod
     def apply(
             self,
             params: np.ndarray
     ) -> None:
-        pass
+        if len(params) != self.params_num:
+            ValueError(
+                f"Invalid parameters shape. "
+                f"Expected {self.params_num}, got {len(params)}."
+            )
+
+        qubits_n = self._func_params_n
+        for r in range(self._reps):
+            params_subset = params[r * qubits_n: (r + 1) * qubits_n]
+            self._variational_func(params=params_subset)
 
 
 class TwoLocal(VariationalForm):
-    """
-    Quantum variational form with a two-local structure.
-
-    Parameters:
-    - n_qubits (int): Number of qubits in the quantum circuit.
-    - rotation_blocks (list[str]): List of rotation gate names. Supported values are 'RX', 'RY', and 'RZ'.
-    - controlled_gate (str): Name of the controlled gate. Default is 'CX' (controlled-X gate).
-    - entanglement (str): Type of entanglement to be applied. Default is 'linear'.
-    - reps (int): Number of repetitions in the variational circuit. Default is 1.
-    """
-
     def __init__(
             self,
             n_qubits: int,
             rotation_blocks: list[str] = None,
             controlled_gate: str = 'CX',
             entanglement: str = 'linear',
-            skip_last_rotation: bool = False,
             reps: int = 1
     ):
         super().__init__(
@@ -118,49 +132,32 @@ class TwoLocal(VariationalForm):
             rotation_blocks=rotation_blocks,
             controlled_gate=controlled_gate,
             entanglement=entanglement,
-            skip_last_rotation=skip_last_rotation,
             reps=reps
         )
 
-    def get_params_num(self) -> int:
-        if not self._skip_last_rotation:
-            self._reps += 1
-        qubits_per_layer = self._n_qubits * len(self._rotation_blocks)
-        return self._reps * qubits_per_layer
-
-    def apply(
+    def _set_variational_func(
             self,
             params: np.ndarray
     ) -> None:
-        for r in range(self._reps):
-            for q in range(self._n_qubits):
-                for j, rot_ in enumerate(self._rotation_blocks):
-                    rot_(params[r * self._n_qubits + q + j], wires=[q])
+        def variational_func():
+            for j, rot_ in enumerate(self._rotation_blocks):
+                for q in range(self._n_qubits):
+                    rot_(params[j * self._n_qubits + q], wires=[q])
+            self._entanglement.apply()
+        return variational_func()
 
 
 class EfficientSU2(TwoLocal):
-    """
-    Quantum variational form with an efficient SU(2) structure.
-
-    Parameters:
-    - n_qubits (int): Number of qubits in the quantum circuit.
-    - rotation_blocks (list[str]): List of rotation gate names. If None, default is ['RY', 'RZ'].
-    - controlled_gate (str): Name of the controlled gate. Default is 'CX' (controlled-X gate).
-    - entanglement (str): Type of entanglement to be applied. Default is 'linear'.
-    - reps (int): Number of repetitions in the variational circuit. Default is 1.
-    """
-
     def __init__(
             self,
             n_qubits: int,
             rotation_blocks: list[str] = None,
             controlled_gate: str = 'CX',
             entanglement: str = 'linear',
-            skip_last_rotation: bool = False,
             reps: int = 1
     ):
         if rotation_blocks is None:
-            self._rotation_blocks = [qml.RY, qml.RX]
+            rotation_blocks = ['RY', 'RX']
         elif len(rotation_blocks) != 2:
             raise ValueError(
                 "EfficientSU2 requires exactly 2 rotation blocks."
@@ -171,34 +168,6 @@ class EfficientSU2(TwoLocal):
             rotation_blocks=rotation_blocks,
             controlled_gate=controlled_gate,
             entanglement=entanglement,
-            skip_last_rotation=skip_last_rotation,
-            reps=reps
-        )
-
-
-class RealAmplitudes(TwoLocal):
-    """
-    Quantum variational form with real amplitudes. Inherits from the 'TwoLocal' class.
-
-    Parameters:
-    - n_qubits (int): Number of qubits in the quantum circuit.
-    - entanglement (str): Type of entanglement to be applied. Default is 'linear'.
-    - reps (int): Number of repetitions in the variational circuit. Default is 1.
-    """
-
-    def __init__(
-            self,
-            n_qubits: int,
-            entanglement: str = 'linear',
-            skip_last_rotation: bool = False,
-            reps: int = 1
-    ):
-        self._rotation_blocks = [qml.RY]
-
-        super().__init__(
-            n_qubits=n_qubits,
-            entanglement=entanglement,
-            skip_last_rotation=skip_last_rotation,
             reps=reps
         )
 
@@ -207,13 +176,8 @@ class TreeTensor(VariationalForm):
     def __init__(
             self,
             n_qubits: int,
-            rotation_blocks: list[str] = None,
-            controlled_gate: str = 'CX',
-            reps: int = 1
+            controlled_gate: str = 'CX'
     ):
-        if rotation_blocks is None:
-            self._rotation_blocks = [qml.RY]
-
         if not (n_qubits & (n_qubits - 1)) == 0:
             raise ValueError(
                 "TreeTensor ansatz requires the number of qubits "
@@ -222,28 +186,43 @@ class TreeTensor(VariationalForm):
 
         super().__init__(
             n_qubits=n_qubits,
-            rotation_blocks=rotation_blocks,
+            rotation_blocks=None,
             controlled_gate=controlled_gate,
-            reps=reps
+            reps=1,
+            uniform_structure=False
         )
 
         self._reps = int(np.log2(n_qubits))
+        self._func_params_n = self.params_num
 
-    def get_params_num(self) -> int:
+    @property
+    def params_num(self) -> int:
         return 2 * self._n_qubits - 1
+
+    def _set_variational_func(
+            self,
+            params: np.ndarray
+    ) -> None:
+        def variational_func():
+            for i in range(self._n_qubits):
+                qml.RY(params[i], wires=[i])
+
+            n_qubits = self._n_qubits
+            for r in range(1, self._reps + 1):
+                for s in range(0, 2 ** (self._reps - r)):
+                    qml.CNOT(wires=[(s * 2 ** r), (s * 2 ** r) + (2 ** (r - 1))])
+                    qml.RY(params[n_qubits + s], wires=[(s * 2 ** r)])
+                n_qubits += 2 ** (self._reps - r)
+
+        return variational_func()
 
     def apply(
             self,
             params: np.ndarray
     ) -> None:
-        self._validate_params_dims(params)
-
-        for i in range(self._n_qubits):
-            qml.RY(params[i], wires=[i])
-
-        n_qubits = self._n_qubits
-        for r in range(1, self._reps + 1):
-            for s in range(0, 2 ** (self._reps - r)):
-                qml.CNOT(wires=[(s * 2 ** r), (s * 2 ** r) + (2 ** (r - 1))])
-                qml.RY(params[n_qubits + s], wires=[(s * 2 ** r)])
-            n_qubits += 2 ** (self._reps - r)
+        if len(params) != self.params_num:
+            ValueError(
+                f"Invalid parameters shape. "
+                f"Expected {self.params_num}, got {len(params)}."
+            )
+        self._variational_func(params=params)
