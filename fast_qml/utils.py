a3a55db53ffc03f0b0c8e6ae2724fc0e2468f1b6
m2papierz <m2papierz@gmail.com>
2024-01-28 23:04:58 +0100

    Minor fix and add docstring to utility function

--- fast_qml/utils.py
+++ fast_qml/utils.py
@@ -7,14 +7,25 @@ def validate_function_args(
         expected_args: list[str]
 ) -> bool:
     """
+    Validates whether the given function's signature matches the expected arguments.
 
     Args:
-        func:
-        expected_args:
+        func: The target function to validate.
+        expected_args: A list of expected argument names.
 
     Returns:
+        True if the function's signature contains all the expected arguments, False otherwise.
 
+    Example:
+        >>> def example_function(param1, param2):
+        ...     pass
+        >>> validate_function_args(example_function, ['param1', 'param2'])
+        True
+        >>> validate_function_args(example_function, ['param1', 'param2', 'param3'])
+        False
+        >>> validate_function_args(example_function, ['param1'])
+        False
     """
     func_signature = inspect.signature(func)
     func_args = list(func_signature.parameters.keys())
-    return set(expected_args) <= set(func_args)
+    return set(expected_args) == set(func_args)
