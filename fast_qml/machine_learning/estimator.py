bd974f27de1c35186ee32fc9e429ce6330f7ad21
m2papierz <m2papierz@gmail.com>
2024-01-29 21:25:26 +0100

    Add docstrings

--- fast_qml/machine_learning/estimator.py
+++ fast_qml/machine_learning/estimator.py
@@ -22,40 +22,59 @@ from fast_qml.machine_learning.optimizer import DefaultOptimizer, JITOptimizer
 
 
 class QuantumEstimator:
+    """
+    Base class for creating quantum estimators.
+
+    This class provides a framework for quantum machine learning models,
+    and is intended to be subclassed for specific implementations.
+
+    Attributes:
+        _n_qubits: Number of qubits in the quantum circuit.
+        _feature_map: The feature map for encoding classical data into quantum states.
+        _ansatz: The variational form (ansatz) for the quantum circuit.
+        _measurement_op: The measurement operator or observable used in the circuit.
+        _loss_fn: The loss function used for optimization.
+        _measurements_num: Number of measurements for estimating expectation values.
+        _interface: The computational interface (e.g., 'auto', 'jax') used by the quantum device.
+        _optimizer: The optimizer used for training the quantum circuit.
+        _device: The quantum device on which the circuit will be executed.
+    """
     def __init__(
             self,
             n_qubits: int,
             feature_map: FeatureMap,
             ansatz: VariationalForm,
             measurement_op: Callable,
-            loss_fn: Callable
+            loss_fn: Callable,
+            measurements_num: int
     ):
         self._n_qubits = n_qubits
         self._feature_map = feature_map
         self._ansatz = ansatz
         self._loss_fn = loss_fn
         self._measurement_op = measurement_op
+        self._measurements_num = measurements_num
 
+        self._setup_device_and_optimizer()
+        self._weights = self._initialize_weights()
+
+    def _setup_device_and_optimizer(self):
+        """ Set up the quantum device and optimizer based on the current device configuration. """
         if fast_qml.DEVICE == QubitDevice.CPU.value:
             self._interface = 'auto'
             self._optimizer = DefaultOptimizer
-            self._device = qml.device(
-                name="default.qubit", wires=n_qubits
-            )
+            self._device = qml.device("default.qubit", wires=self._n_qubits)
         elif fast_qml.DEVICE == QubitDevice.CPU_JAX.value:
             self._interface = 'jax'
             self._optimizer = JITOptimizer
             self._optimizer.register_pytree_node()
-            self._device = qml.device(
-                name="default.qubit.jax", wires=n_qubits
-            )
+            self._device = qml.device("default.qubit.jax", wires=self._n_qubits)
         else:
-            raise NotImplementedError()
-
-        self._weights = self._initialize_weights()
+            raise NotImplementedError("The specified device is not supported.")
 
     @abstractmethod
     def _initialize_weights(self) -> np.ndarray:
+        """ Initialize weights for the quantum circuit. """
         pass
 
     @abstractmethod
@@ -64,6 +83,7 @@ class QuantumEstimator:
             weights: np.ndarray,
             x_data: np.ndarray
     ):
+        """ Define and apply the quantum layer of the circuit. """
         pass
 
     @abstractmethod
@@ -72,6 +92,7 @@ class QuantumEstimator:
             weights: np.ndarray,
             x_data: np.ndarray
     ):
+        """ Define and apply the quantum model for the variational estimator. """
         pass
 
     @abstractmethod
@@ -81,6 +102,8 @@ class QuantumEstimator:
             y_data: np.ndarray,
             learning_rate: float,
             num_epochs: int,
+            batch_size: int,
             verbose: bool
     ):
+        """ Fit the model to the given data. """
         pass
