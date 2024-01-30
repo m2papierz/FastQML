25e96d1ff01adc5ca44373ee08c856914b91507e
m2papierz <m2papierz@gmail.com>
2024-01-30 09:33:04 +0100

    Adjust CrossEntropyLoss to both one-hot and index targets, add docstrings

--- fast_qml/machine_learning/loss_functions.py
+++ fast_qml/machine_learning/loss_functions.py
@@ -8,6 +8,28 @@
 #
 # THERE IS NO WARRANTY for the FastQML library, as per Section 15 of the GPL v3.
 
+"""
+Loss Functions Module.
+
+This module provides implementations of various loss functions used in machine learning. The classes in this
+module are intended to be used as loss functions in training loops of machine learning models. An instance of
+any of these classes can be created and passed to optimization algorithms to compute the loss between
+predicted and actual values.
+
+Example:
+    >>> loss_fn = MSELoss()
+    >>> loss = loss_fn(y_real, y_pred)
+    >>> print("MSE Loss:", loss)
+
+    >>> huber_loss_fn = HuberLoss(delta=1.0)
+    >>> loss = huber_loss_fn(y_real, y_pred)
+    >>> print("Huber Loss:", loss)
+
+    >>> logcosh_loss_fn = LogCoshLoss()
+    >>> loss = logcosh_loss_fn(y_real, y_pred)
+    >>> print("LogCosh Loss:", loss)
+"""
+
 import importlib
 from abc import abstractmethod
 
@@ -38,6 +60,11 @@ class LossFunction:
 
     @staticmethod
     def _get_numpy_module():
+        """
+        Determines the appropriate numpy module to use based on the quantum device configuration.
+        This method checks the current configuration set in fast_qml.DEVICE and returns
+        the corresponding.
+        """
         if fast_qml.DEVICE == QubitDevice.CPU.value:
             return importlib.import_module('pennylane.numpy')
         elif fast_qml.DEVICE == QubitDevice.CPU_JAX.value:
@@ -51,17 +78,47 @@ class LossFunction:
             y_real: np.ndarray,
             y_pred: np.ndarray
     ) -> float:
-        pass
+        """
+        Abstract method for computing the loss. This method must be implemented by subclasses.
+
+        The method takes in the true values (y_real) and the predicted values (y_pred) and
+        computes the loss between them. The specific computation depends on the loss function
+        implemented by the subclass.
+
+        Parameters:
+            y_real: The true values.
+            y_pred: The predicted values.
+
+        Returns:
+            The computed loss.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
 
     def __call__(
             self,
             y_real: np.ndarray,
             y_pred: np.ndarray
     ) -> float:
+        """
+        Enables the LossFunction object to be called as a function. This method provides a convenient
+        way to compute the loss using the implemented `_loss_fn` method.
+
+        Args:
+            y_real: The true values.
+            y_pred: The predicted values.
+
+        Returns:
+            The computed loss based on the logic defined in the `_loss_fn` method.
+        """
         return self._loss_fn(y_real, y_pred)
 
 
 class MSELoss(LossFunction):
+    """
+    This class implements the Mean Squared Error (MSE) loss function. The MSE loss calculates
+    the average of the squares of the differences between the predicted values (y_pred) and the
+    actual values (y_real). This loss function is widely used in regression problems.
+    """
     def __init__(self):
         super().__init__()
 
@@ -70,11 +127,26 @@ class MSELoss(LossFunction):
             y_real: np.ndarray,
             y_pred: np.ndarray
     ) -> float:
+        """
+        Calculate the Mean Squared Error loss.
+
+        Args:
+            y_real: The actual values.
+            y_pred: The predicted values.
+
+        Returns:
+            The computed MSE loss.
+        """
         loss = self._np_module.sum((y_real - y_pred) ** 2) / len(y_real)
         return loss
 
 
 class HuberLoss(LossFunction):
+    """
+    This class implements the Huber loss function, which is less sensitive to outliers in data
+    than the squared error loss. The Huber loss is quadratic for small values of error and linear
+    for large values, with the transition point set by the 'delta' parameter.
+    """
     def __init__(self, delta: float = 1.0):
         super().__init__()
         self.delta = delta
@@ -84,6 +156,16 @@ class HuberLoss(LossFunction):
             y_real: np.ndarray,
             y_pred: np.ndarray
     ) -> float:
+        """
+        Calculate the Huber loss.
+
+        Args:
+            y_real: The actual values.
+            y_pred: The predicted values.
+
+        Returns:
+            float: The computed Huber loss.
+        """
         error = y_real - y_pred
         huber_loss = self._np_module.where(
             self._np_module.abs(error) < self.delta, 0.5 * error ** 2,
@@ -94,6 +176,13 @@ class HuberLoss(LossFunction):
 
 
 class LogCoshLoss(LossFunction):
+    """
+    This class implements the Log-Cosh loss function, which is a smooth version of the L2 norm that
+    is less influenced by occasional wildly incorrect predictions.
+
+    Log-Cosh is the logarithm of the hyperbolic cosine of the prediction error and is mainly used
+    in regression problems.
+    """
     def __init__(self):
         super().__init__()
 
@@ -102,12 +191,30 @@ class LogCoshLoss(LossFunction):
             y_real: np.ndarray,
             y_pred: np.ndarray
     ) -> float:
+        """
+        Calculate the Log-Cosh loss.
+
+        Args:
+            y_real: The actual values.
+            y_pred: The predicted values.
+
+        Returns:
+            The computed Log-Cosh loss.
+        """
         log_cosh_loss = self._np_module.log(np.cosh(y_real - y_pred))
         loss = self._np_module.mean(log_cosh_loss)
         return loss
 
 
 class BinaryCrossEntropyLoss(LossFunction):
+    """
+    BinaryCrossEntropyLoss implements a binary cross-entropy loss function for binary
+    classification tasks.
+
+    IT is specifically tailored for scenarios where the output can be classified into
+    one of two possible classes (binary classification). It calculates the loss between
+    the true binary labels and the predicted probabilities.
+    """
     def __init__(self, eps: float = 1e-12):
         super().__init__()
         self._eps = eps
@@ -125,15 +232,39 @@ class BinaryCrossEntropyLoss(LossFunction):
 
 
 class CrossEntropyLoss(LossFunction):
+    """
+    CrossEntropyLoss implements a loss function for classification tasks, specifically using the
+    cross-entropy loss metric. This class is intended for multi-class classification problems.
+
+    It supports both one-hot encoded targets and class index targets.
+    """
     def __init__(self, eps: float = 1e-12):
         super().__init__()
         self._eps = eps
+
+    def _one_hot_encode(self, y_real, k):
+        """ Creates a one-hot encoding of x of size k """
+        return self._np_module.array(y_real[:, None] == self._np_module.arange(k))
+
+    def _is_one_hot(self, y_real):
+        """ Checks if y is one-hot encoded """
+        return (
+                y_real.ndim == 2 and
+                self._np_module.all(self._np_module.sum(y_real, axis=1) == 1) and
+                self._np_module.all((y_real == 0) | (y_real == 1))
+        )
 
     def _loss_fn(
             self,
             y_real: np.ndarray,
             y_pred: np.ndarray
     ) -> float:
+        if y_pred.shape[0] != len(y_real):
+            y_pred = y_pred.transpose()
+
+        if not self._is_one_hot(y_real):
+            y_real = self._one_hot_encode(y_real, y_pred.shape[-1])
+
         y_pred = self._np_module.clip(y_pred, self._eps, 1 - self._eps)
         loss = -self._np_module.sum(y_real * self._np_module.log(y_pred))
         loss /= len(y_real)
