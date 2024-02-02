f79ce66aa674cfe469c635f992c54cbc24d5ab41
m2papierz <m2papierz@gmail.com>
2024-02-02 13:55:02 +0100

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FastQML',
+    version='0.1',
+    author='Mateusz Papierz',
+    author_email='m2papierz@gmail.com',
+    description='A Python library for Quantum Machine Learning, designed for easy and scalable '
+                'implementation of QML models, optimized for CPU and GPU.',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'numpy',
+        'pennylane',
+        'jax',
+        'optax'
+    ],
+    url='https://github.com/m2papierz/FastQML',
+    license='GNU General Public License v3.0'
+)
