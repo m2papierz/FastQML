c8214364207398a72355425aa6b894511d682c9c
m2papierz <m2papierz@gmail.com>
2024-02-10 00:29:55 +0100

    Update docstrings

--- fast_qml/machine_learning/estimators/hybrid_qnn.py
+++ fast_qml/machine_learning/estimators/hybrid_qnn.py
@@ -13,7 +13,6 @@ from typing import (
 
 import jax
 import flax.linen as nn
-import pennylane as qml
 from jax import numpy as jnp
 
 from fast_qml.machine_learning.estimator import HybridEstimator
@@ -22,6 +21,25 @@ from fast_qml.machine_learning.estimators.vqa import VQRegressor, VQClassifier
 
 
 class HybridModel(HybridEstimator):
+    """
+    Base class for hybrid quantum-classical machine learning models.
+
+    Inherits from HybridEstimator and facilitates the integration of classical neural network architectures
+    with quantum computing models to create powerful hybrid models. This approach aims to leverage the
+    computational benefits of quantum processing for specific tasks within a broader machine learning workflow,
+    potentially improving performance on tasks where quantum computing offers an advantage.
+
+    The HybridModel class is designed to be flexible and extendable, serving as a foundation for building
+    various types of hybrid models tailored to specific applications.
+
+    Args:
+        input_shape: The shape of the input data for the classical component of the hybrid model.
+        c_model: The classical model component.
+        q_model: The quantum model component, defined as an instance of a QuantumEstimator subclass.
+        loss_fn: The loss function for evaluating the hybrid model's performance.
+        optimizer: The optimization algorithm.
+        batch_norm: Indicates the use of batch normalization in the classical component of the model.
+    """
     def __init__(
             self,
             input_shape,
@@ -43,13 +61,14 @@ class HybridModel(HybridEstimator):
     def _initialize_parameters(
             self,
             input_shape: Union[int, Tuple[int]],
-            batch_norm: bool = False
+            batch_norm: bool
     ) -> Dict[str, Any]:
         """
-        Initializes weights for the classical and quantum models.
+        Initializes parameters for classical and quantum models.
 
         Args:
             input_shape: The shape of the input data.
+            batch_norm: Indicates whether batch normalization is used within the model.
 
         Returns:
             A dictionary containing initialized weights for both classical and quantum models.
@@ -79,8 +98,8 @@ class HybridModel(HybridEstimator):
             self,
             c_weights: Dict[str, Mapping[str, jnp.ndarray]],
             q_weights: jnp.ndarray,
-            x_data: jnp.ndarray,
             batch_stats: Union[Dict[str, Mapping[str, jnp.ndarray]], None],
+            x_data: jnp.ndarray,
             training: bool
     ):
         """
@@ -89,7 +108,9 @@ class HybridModel(HybridEstimator):
         Args:
             c_weights: Weights of the classical model.
             q_weights: Weights of the quantum model.
+            batch_stats: Batch statistics for batch normalization.
             x_data: Input data for the model.
+            training: Boolean flag indicating if training model inference.
 
         Returns:
             The output of the hybrid model.
@@ -130,8 +151,10 @@ class HybridRegressor(HybridModel):
         input_shape: The shape of the input data.
         c_model: The classical neural network model.
         q_model: The quantum regression model.
+        loss_fn: The loss function used to evaluate the model.
+        optimizer: The optimization algorithm.
+        batch_norm: Indicates whether batch normalization is used within the model.
     """
-
     def __init__(
             self,
             input_shape,
@@ -175,6 +198,20 @@ class HybridRegressor(HybridModel):
 
 
 class HybridClassifier(HybridModel):
+    """
+    A hybrid quantum-classical classifier for classification tasks.
+
+    This class extends the HybridEstimator for classification tasks using quantum neural networks
+    or variational quantum algorithms combined with classical neural networks.
+
+    Args:
+        input_shape: The shape of the input data.
+        c_model: The classical neural network model.
+        q_model: The quantum classifier model.
+        loss_fn: The loss function used to evaluate the model.
+        optimizer: The optimization algorithm.
+        batch_norm: Indicates whether batch normalization is used within the model.
+    """
     def __init__(
             self,
             input_shape,
