c599675bcfb841d45da81ad8dd29b182938773be
m2papierz <m2papierz@gmail.com>
2024-02-09 16:29:11 +0100

    Refactor HybridEstimator for new HybridOptimizer interface

--- fast_qml/machine_learning/estimator.py
+++ fast_qml/machine_learning/estimator.py
@@ -23,11 +23,9 @@ from torch.utils.data import DataLoader
 from fast_qml.quantum_circuits.feature_maps import FeatureMap
 from fast_qml.quantum_circuits.variational_forms import VariationalForm
 from fast_qml.core.callbacks import EarlyStopping
-from fast_qml.machine_learning.optimizer import HybridOptimizer
 
-from fast_qml.core.optimizer import QuantumOptimizer
-from fast_qml.core.opt_classical import ClassicalOptimizer
-
+from fast_qml.core.optimizer import (
+    QuantumOptimizer, ClassicalOptimizer, HybridOptimizer)
 
 class QuantumEstimator:
     """
@@ -191,41 +189,21 @@ class ClassicalEstimator:
 
     def fit(
             self,
-            train_data: np.ndarray,
-            train_targets: np.ndarray,
-            val_data: np.ndarray,
-            val_targets: np.ndarray,
+            train_data: Union[np.ndarray, torch.Tensor, DataLoader],
+            val_data: Union[np.ndarray, torch.Tensor, DataLoader],
+            train_targets: Union[np.ndarray, torch.Tensor, None] = None,
+            val_targets: Union[np.ndarray, torch.Tensor, None] = None,
             learning_rate: float = 0.01,
             num_epochs: int = 500,
             batch_size: int = None,
             early_stopping: EarlyStopping = None,
             verbose: bool = True
     ) -> None:
-        """
-        Trains the hybrid classical-quantum model on the given data.
-
-        This method optimizes the weights of the hybrid model using the specified loss function
-        and optimizer. It updates the weights based on the training data over a number of epochs.
-
-        Args:
-            train_data: Input features for training.
-            train_targets: Target outputs for training.
-            val_data: Input features for validation.
-            val_targets: Target outputs for validation.
-            learning_rate: Learning rate for the optimizer.
-            num_epochs: Number of epochs to run the training.
-            batch_size: Size of batches for training. If None, the whole dataset is used in each iteration.
-            early_stopping: Instance of EarlyStopping to be used during training.
-            verbose : If True, prints verbose messages during training.
-
-        If early stopping is configured and validation data is provided, the training process will
-        stop early if no improvement is seen in the validation loss for a specified number of epochs.
-        """
         if self._batch_norm:
             weights, batch_stats = (
                 self._params['weights'], self._params['batch_stats'])
         else:
-            weights, batch_stats = self._params, None
+            weights, batch_stats = self._params['weights'], None
 
         optimizer = ClassicalOptimizer(
             c_params=weights,
@@ -251,134 +229,87 @@ class ClassicalEstimator:
 
 
 class HybridEstimator:
-    """
-    Base class for creating hybrid quantum-classical machine learning models.
-
-    This class combines classical neural network models with quantum neural networks or variational
-    quantum algorithms to form a hybrid model for regression or classification tasks.
-
-    Attributes:
-        _c_model: The classical neural network model.
-        _q_model: The quantum model, which can be either a quantum neural network or a variational quantum algorithm.
-        _optimizer: Optimizer for training the hybrid model.
-        _inp_rng: Random number generator key for input initialization.
-        _init_rng: Random number generator key for model initialization.
-        _weights: Initialized weights for both classical and quantum models.
-
-    Args:
-        input_shape: The shape of the input data.
-        c_model: The classical neural network model.
-        q_model: The quantum model.
-    """
     def __init__(
             self,
             input_shape,
-            c_model,
-            q_model,
+            c_model: nn.Module,
+            q_model: QuantumEstimator,
+            loss_fn: Callable,
+            batch_norm: bool
     ):
         self._c_model = c_model
         self._q_model = q_model
-
-        self._optimizer = HybridOptimizer
+        self._loss_fn = loss_fn
+        self._batch_norm = batch_norm
 
         self._inp_rng, self._init_rng = jax.random.split(
             jax.random.PRNGKey(seed=42), num=2)
-        self._weights = self._initialize_weights(input_shape)
+        self._params = self._initialize_parameters(
+            input_shape=input_shape, batch_norm=batch_norm)
 
-    def _initialize_weights(
+    @abstractmethod
+    def _initialize_parameters(
             self,
-            input_shape: Union[int, Tuple[int]]
+            input_shape: Union[int, Tuple[int]],
+            batch_norm: bool = False
     ) -> Dict[str, Any]:
         """
         Initializes weights for the classical and quantum models.
-
-        Args:
-            input_shape: The shape of the input data.
-
-        Returns:
-            A dictionary containing initialized weights for both classical and quantum models.
         """
-        c_inp = jax.random.normal(self._inp_rng, shape=input_shape)
-        c_weights = self._c_model.init(self._init_rng, c_inp)
-        return {
-            'c_weights': c_weights,
-            'q_weights': self._q_model.weights
-        }
+        raise NotImplementedError("Subclasses must implement this method.")
 
+    @abstractmethod
     def _model(
             self,
             c_weights: Dict[str, Mapping[str, jnp.ndarray]],
             q_weights: jnp.ndarray,
-            x_data: jnp.ndarray
+            x_data: jnp.ndarray,
+            batch_stats: Union[Dict[str, Mapping[str, jnp.ndarray]], None],
+            training: bool
     ):
         """
         Defines the hybrid model by combining classical and quantum models.
-
-        Args:
-            c_weights: Weights of the classical model.
-            q_weights: Weights of the quantum model.
-            x_data: Input data for the model.
-
-        Returns:
-            The output of the hybrid model.
         """
-        def _hybrid_model():
-            c_out = self._c_model.apply(c_weights, x_data)
-            c_out = jax.numpy.array(c_out)
-            q_out = self._q_model.q_model(weights=q_weights, x_data=c_out)
-            return q_out
-
-        return _hybrid_model()
+        raise NotImplementedError("Subclasses must implement this method.")
 
     def fit(
             self,
-            x_train: jnp.ndarray,
-            y_train: jnp.ndarray,
-            x_val: jnp.ndarray = None,
-            y_val: jnp.ndarray = None,
+            train_data: Union[np.ndarray, torch.Tensor, DataLoader],
+            val_data: Union[np.ndarray, torch.Tensor, DataLoader],
+            train_targets: Union[np.ndarray, torch.Tensor, None] = None,
+            val_targets: Union[np.ndarray, torch.Tensor, None] = None,
             learning_rate: float = 0.01,
             num_epochs: int = 500,
             batch_size: int = None,
             early_stopping: EarlyStopping = None,
             verbose: bool = True
     ) -> None:
-        """
-        Trains the hybrid classical-quantum model on the given data.
 
-        This method optimizes the weights of the hybrid model using the specified loss function
-        and optimizer. It updates the weights based on the training data over a number of epochs.
+        if self._batch_norm:
+            c_weights, batch_stats = (
+                self._params['c_weights'], self._params['batch_stats'])
+        else:
+            c_weights, batch_stats = self._params['c_weights'], None
+        q_weights = self._params['q_weights']
 
-        Args:
-            x_train: Input features for training.
-            y_train: Target outputs for training.
-            x_val: Input features for validation.
-            y_val: Target outputs for validation.
-            learning_rate: Learning rate for the optimizer.
-            num_epochs: Number of epochs to run the training.
-            batch_size: Size of batches for training. If None, the whole dataset is used in each iteration.
-            early_stopping: Instance of EarlyStopping to be used during training.
-            verbose : If True, prints verbose messages during training.
-
-        If early stopping is configured and validation data is provided, the training process will
-        stop early if no improvement is seen in the validation loss for a specified number of epochs.
-        """
-        optimizer = self._optimizer(
-            c_params=self._weights['c_weights'],
-            q_params=self._weights['q_weights'],
+        optimizer = HybridOptimizer(
+            c_params=c_weights,
+            q_params=q_weights,
+            batch_stats=batch_stats,
             model=self._model,
             loss_fn=self._q_model.loss_fn,
             batch_size=batch_size,
-            epochs_num=num_epochs,
             learning_rate=learning_rate,
             early_stopping=early_stopping
         )
 
         optimizer.optimize(
-            x_train=jnp.array(x_train),
-            y_train=jnp.array(y_train),
-            x_val=jnp.array(x_val),
-            y_val=jnp.array(y_val),
+            train_data=train_data,
+            train_targets=train_targets,
+            val_data=val_data,
+            val_targets=val_targets,
+            epochs_num=num_epochs,
             verbose=verbose
         )
 
-        self._weights['c_weights'], self._weights['q_weights'] = optimizer.weights
+        self._params['c_weights'], self._params['q_weights'] = optimizer.weights
