Adjust fisher information matrix calculation for estimator parameters sampling and correct normalization

fast_qml/circuit_analysis/fisher_information.py
@@ -12,8 +12,6 @@
 An implementation of the Fisher Information Matrix (FIM).
 """
 
-from functools import partial
-from typing import Dict, Mapping
 from dataclasses import asdict
 
 import jax
@@ -33,15 +31,11 @@ class FisherInformation:
             self,
             estimator: Estimator
     ):
-        self._estimator_model = estimator.model
-        self._model_params = estimator.params
+        self._estimator = estimator
 
     def _get_proba_and_grads(
             self,
-            x: jnp.ndarray,
-            q_params: jnp.ndarray,
-            c_params: Dict[str, Mapping[str, jnp.ndarray]],
-            batch_stats: Dict[str, Mapping[str, jnp.ndarray]]
+            x: jnp.ndarray
     ):
         """
         Computes the output probabilities and their gradients with respect to the model
@@ -49,54 +43,44 @@ class FisherInformation:
 
         Args:
             x: The input data for which the Fisher Information Matrix is to be computed.
-            q_params: Parameters of the quantum model.
-            c_params: Parameters of the classical model.
-            batch_stats: Batch normalization statistics for the classical model.
 
         Returns:
             A tuple containing two elements:
             - jnp.ndarray representing the output probabilities of the model for the given input.
             - jnp.ndarray representing the gradients of the output probabilities.
         """
+        # Sample new set of model parameters and unpack them
+        self._estimator.init_parameters()
+        c_params, q_params, batch_stats = asdict(self._estimator.params).values()
+
         # Compute model output probabilities
-        proba = self._estimator_model(
+        proba = self._estimator.model(
             x_data=x, q_weights=q_params,
             c_weights=c_params, batch_stats=batch_stats,
             training=False, q_model_probs=True)
 
         # Compute derivatives of probabilities in regard to model parameters
         proba_d = jax.jacfwd(
-            self._estimator_model)(x, q_params, c_params, batch_stats, False, True)
+            self._estimator.model)(x, q_params, c_params, batch_stats, False, True)
 
         return proba, proba_d
 
     def _compute_fisher_matrix(
             self,
-            x: jnp.ndarray,
-            q_params: jnp.ndarray,
-            c_params: Dict[str, Mapping[str, jnp.ndarray]],
-            batch_stats: Dict[str, Mapping[str, jnp.ndarray]]
+            x: jnp.ndarray
     ) -> jnp.ndarray:
         """
         Computes the Fisher Information Matrix for a given input.
 
-        This method computes the Fisher Information Matrix (FIM) for the model parameters based on the input.
-        The FIM is a way of estimating the amount of information that an observable random variable carries
-        about an unknown parameter upon which the probability of the random variable depends.
-
         Args:
             x: The input data for which the Fisher Information Matrix is to be computed.
-            q_params: Parameters of the quantum model.
-            c_params: Parameters of the classical model.
-            batch_stats: Batch normalization statistics for the classical model.
 
         Returns:
             The Fisher Information Matrix, a square array of shape (n_params, n_params), where
             `n_params` is the number of model outputs (observables).
         """
         # Compute probabilities and gradients
-        proba, proba_d = self._get_proba_and_grads(
-            x=x, q_params=q_params, c_params=c_params, batch_stats=batch_stats)
+        proba, proba_d = self._get_proba_and_grads(x=x)
 
         # Exclude zero values and calculate 1 / proba
         non_zeros_proba = qml.math.where(
@@ -126,17 +110,17 @@ class FisherInformation:
         Returns:
             The normalized Fisher Information Matrix, averaged over the input batch of data.
         """
-        # Unpack model parameters
-        c_params, q_params, batch_stats = asdict(self._model_params).values()
+
 
         # Create batched version of fisher matrix computation
         _compute_fisher_matrix_batched = vmap(
-            self._compute_fisher_matrix, in_axes=(0, None, None, None))
+            self._compute_fisher_matrix, in_axes=0)
 
         # Compute FIM average over the given data
-        fim = jnp.mean(_compute_fisher_matrix_batched(
-            x_data, q_params, c_params, batch_stats), axis=0)
+        fim = jnp.mean(_compute_fisher_matrix_batched(x_data), axis=0)
 
-        fisher_inf_norm = (fim - jnp.min(fim)) / (jnp.max(fim) - jnp.min(fim))
+        # Normalize FIM
+        estimator_params_num = self._estimator.params.params_num
+        fisher_inf_norm = fim * estimator_params_num / jnp.trace(fim)
 
         return fisher_inf_norm