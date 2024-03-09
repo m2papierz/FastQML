5ae36e7e341b793753224cc561b0532383433172
m2papierz <m2papierz@gmail.com>
2024-03-09 13:30:52 +0100

    Add docstrings

--- fast_qml/circuit_analysis/fisher_information.py
+++ fast_qml/circuit_analysis/fisher_information.py
@@ -8,6 +8,12 @@
 #
 # THERE IS NO WARRANTY for the FastQML library, as per Section 15 of the GPL v3.
 
+"""
+An implementation of the Fisher Information Matrix (FIM).
+"""
+
+from functools import partial
+from typing import Dict, Mapping
 from dataclasses import asdict
 
 import jax
@@ -18,6 +24,11 @@ from jax import vmap
 from fast_qml.core.estimator import Estimator
 
 class FisherInformation:
+    """
+    Class encapsulating computation of the Fisher Information Matrix (FIM).
+
+    See:  Abbas et al., "The power of quantum neural networks." <https://arxiv.org/pdf/2011.00027.pdf>
+    """
     def __init__(
             self,
             estimator: Estimator
@@ -25,26 +36,27 @@ class FisherInformation:
         self._estimator_model = estimator.model
         self._model_params = estimator.params
 
-    def _compute_fisher_matrix(
+    def _get_proba_and_grads(
             self,
             x: jnp.ndarray,
-            q_params,
-            c_params,
-            batch_stats
-    ) -> jnp.ndarray:
+            q_params: jnp.ndarray,
+            c_params: Dict[str, Mapping[str, jnp.ndarray]],
+            batch_stats: Dict[str, Mapping[str, jnp.ndarray]]
+    ):
         """
-        Computes the Fisher Information Matrix for a given input.
-
-        This method computes the Fisher Information Matrix (FIM) for the model parameters based on the input.
-        The FIM is a way of estimating the amount of information that an observable random variable carries
-        about an unknown parameter upon which the probability of the random variable depends.
+        Computes the output probabilities and their gradients with respect to the model
+        parameters for a given input.
 
         Args:
             x: The input data for which the Fisher Information Matrix is to be computed.
+            q_params: Parameters of the quantum model.
+            c_params: Parameters of the classical model.
+            batch_stats: Batch normalization statistics for the classical model.
 
         Returns:
-            The Fisher Information Matrix, a square array of shape (n_params, n_params), where
-            `n_params` is the number of model outputs (observables).
+            A tuple containing two elements:
+            - jnp.ndarray representing the output probabilities of the model for the given input.
+            - jnp.ndarray representing the gradients of the output probabilities.
         """
         # Compute model output probabilities
         proba = self._estimator_model(
@@ -55,6 +67,36 @@ class FisherInformation:
         # Compute derivatives of probabilities in regard to model parameters
         proba_d = jax.jacfwd(
             self._estimator_model)(x, q_params, c_params, batch_stats, False, True)
+
+        return proba, proba_d
+
+    def _compute_fisher_matrix(
+            self,
+            x: jnp.ndarray,
+            q_params: jnp.ndarray,
+            c_params: Dict[str, Mapping[str, jnp.ndarray]],
+            batch_stats: Dict[str, Mapping[str, jnp.ndarray]]
+    ) -> jnp.ndarray:
+        """
+        Computes the Fisher Information Matrix for a given input.
+
+        This method computes the Fisher Information Matrix (FIM) for the model parameters based on the input.
+        The FIM is a way of estimating the amount of information that an observable random variable carries
+        about an unknown parameter upon which the probability of the random variable depends.
+
+        Args:
+            x: The input data for which the Fisher Information Matrix is to be computed.
+            q_params: Parameters of the quantum model.
+            c_params: Parameters of the classical model.
+            batch_stats: Batch normalization statistics for the classical model.
+
+        Returns:
+            The Fisher Information Matrix, a square array of shape (n_params, n_params), where
+            `n_params` is the number of model outputs (observables).
+        """
+        # Compute probabilities and gradients
+        proba, proba_d = self._get_proba_and_grads(
+            x=x, q_params=q_params, c_params=c_params, batch_stats=batch_stats)
 
         # Exclude zero values and calculate 1 / proba
         non_zeros_proba = qml.math.where(
@@ -94,6 +136,7 @@ class FisherInformation:
         # Compute FIM average over the given data
         fim = jnp.mean(_compute_fisher_matrix_batched(
             x_data, q_params, c_params, batch_stats), axis=0)
+
         fisher_inf_norm = (fim - jnp.min(fim)) / (jnp.max(fim) - jnp.min(fim))
 
         return fisher_inf_norm
