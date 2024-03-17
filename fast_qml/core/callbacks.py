b9b4b418ca70d06e340c3e7064a53170c5f3d435
m2papierz <m2papierz@gmail.com>
2024-03-17 22:52:05 +0100

    Adjust callback to new Estimator and ParametersOptimizer interface

--- fast_qml/core/callbacks.py
+++ fast_qml/core/callbacks.py
@@ -8,7 +8,7 @@
 #
 # THERE IS NO WARRANTY for the FastQML library, as per Section 15 of the GPL v3.
 
-from jax import numpy as jnp
+from collections import OrderedDict
 
 
 class EarlyStopping:
@@ -69,59 +69,40 @@ class EarlyStopping:
 
 class BestModelCheckpoint:
     """
-    A callback class for tracking and storing the best model parameters based on validation loss during
-    the training loop.
-
-    This class is designed to be used with an optimizer during the training process. It keeps track of the
-    best model parameters when provided with the current model parameters and validation loss at each epoch.
+    A callback class for tracking and storing the best estimator parameters based on validation
+    loss during the training loop. This class is designed to be used with an optimizer during the
+    training process. It keeps track of the best model parameters when provided with the current
+    estimator parameters and validation loss at each epoch.
 
     Attributes:
-        best_q_params: Stores the best quantum model parameters if provided during training.
-        best_c_params: Stores the best classical model parameters if provided during training.
-        best_batch_stats: Stores the best batch statistics if provided during training
+        best_params: Stores the best estimator parameters.
         best_val_loss: Records the lowest validation loss encountered during training.
     """
     def __init__(self):
-        self.best_q_params = None
-        self.best_c_params = None
-        self.best_batch_stats = None
+        self.best_params = None
         self.best_val_loss = float('inf')
 
     def update(
             self,
             current_val_loss: float,
-            current_c_params: jnp.ndarray = None,
-            current_q_params: jnp.ndarray = None,
-            current_batch_stats: jnp.ndarray = None
+            current_params: OrderedDict = None,
     ) -> None:
         """
         Updates the best model parameters if the current validation loss is lower.
 
         Args:
             current_val_loss: Current validation loss.
-            current_q_params: Current quantum model parameters.
-            current_c_params: Current classical model parameters.
-            current_batch_stats: Current batch statistics
+            current_params: Current estimator model parameters.
         """
         if current_val_loss < self.best_val_loss:
-            if current_q_params is not None:
-                self.best_q_params = current_q_params
-            if current_c_params is not None:
-                self.best_c_params = current_c_params
-            if current_batch_stats is not None:
-                self.best_batch_stats = current_batch_stats
+            self.best_params = current_params
             self.best_val_loss = current_val_loss
 
     def load_best_model(self, optimizer) -> None:
         """
-        Loads the best model parameters into the optimizer.
+        Loads the best estimator parameters into the optimizer.
 
         Args:
             optimizer: The optimizer instance to update.
         """
-        if self.best_q_params is not None:
-            optimizer._q_params = self.best_q_params
-        if self.best_c_params is not None:
-            optimizer._c_params = self.best_c_params
-        if self.best_batch_stats is not None:
-            optimizer._batch_stats = self.best_batch_stats
+        optimizer._parameters = self.best_params
