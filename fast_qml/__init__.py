d753e9287238de296333a0ef589f4e176829bfac
m2papierz <m2papierz@gmail.com>
2024-03-18 17:13:43 +0100

    Remove redundant update of jax configuration

--- fast_qml/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from jax import config as j_cfg
-except ImportError:
-    raise ImportError(
-        "JAX is not installed. Please install JAX before using FastQML."
-    )
-
-j_cfg.update("jax_enable_x64", True)
